3d3e636d26ef0a2130acfa5aa555e9309518ad9f
John Simpson <john@swajime.com>
2020-06-30 11:49:39 -0400

    Update and rename test_swaANSI.py to test_SwaANSI.py

--- tests/test_SwaANSI.py
+++ tests/test_SwaANSI.py
@@ -2,21 +2,21 @@
 #
 
 import unittest
-from swajime import swaANSI
+from swajime import SwaANSI
 
 
 class TestSwaANSI(unittest.TestCase):
 
     def test___init__(self):
-        self.assertGreater(len(swaANSI.colors), 200, 'Verify 256 colors available.')     
-        self.assertEqual(len(swaANSI.styles), 10, 'Verify 10 styles available.')     
+        self.assertGreater(len(SwaANSI.colors), 200, 'Verify 256 colors available.')     
+        self.assertEqual(len(SwaANSI.styles), 10, 'Verify 10 styles available.')     
 
     def test_class_display(self):
-        redTest = swaANSI.display("test", "RED")
+        redTest = SwaANSI.display("test", "RED")
         self.assertEqual(redTest, "\033[38;5;9mtest\033[0m")
 
     def test_object_display(self):
-        redTest = swaANSI("RED")
+        redTest = SwaANSI("RED")
         self.assertEqual(redTest.display('test'), "\033[38;5;9mtest\033[0m")
 
 
