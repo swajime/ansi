06175c60a7e566dd1d2e719de781990e0addab53
John Simpson <john@swajime.com>
2020-07-01 12:34:51 -0400

    SwaANSI 0.0.4 - Added setup.py for publishing.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# swa-ansi
-Enable setting colors and attributes on text strings
+# SwaANSI
+
+<p>Enable wrapping text with color and attributes per ANSI escape sequences.</p>
+![Python package](https://github.com/swajime/ansi/workflows/Python%20package/badge.svg)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SwaANSI-pkg-swajime",
+    version="0.0.4",
+    author="John Simpson",
+    author_email="john@swajime.com",
+    description="Enable wrapping text with color and attributes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swajime/ansi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
